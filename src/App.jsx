4ad0e3823c34c6d5ed86e7a86ef18a1884f40fbe
Davor <davor.kos@gmail.com>
2023-02-05 01:30:09 +0100

    taskmate first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ClassComponent from "./ClassComponent";
+import FuncComponent from "./FuncComponent";
+
+export const App = () => {
+    return (
+        <>
+            Pozdrav iz App.jsx komponente!
+            <ClassComponent />
+            <FuncComponent />
+        </>
+    );
+};
+
+export default App;
---
+++ src/FuncComponent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FuncComponent() {
+    return <div>Ovo je Function based Component!</div>;
+}
